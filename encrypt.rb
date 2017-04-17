d7f08f497e176f0f3488436bd66c71585a843bb3
fernal9301 <ivanovsergey1988@gmail.com>
2017-04-17 22:29:25 +0300

    add method for sha2 hash alg

--- encrypt.rb
+++ encrypt.rb
@@ -2,16 +2,19 @@
 require 'digest'
 puts "Введите слово или фразу для шифрования: "
 word = STDIN.gets.chomp
-user_choice = ''
-until user_choice == '1' or user_choice == '2'
+user_choice = 0
+until user_choice.between?(1, 3)
   puts "Каким способом зашифровать:
   1. MD5
-  2. SHA1"
-  user_choice = STDIN.gets.chomp
+  2. SHA1
+  3. SHA2"
+  user_choice = STDIN.gets.to_i
 end
 case user_choice
-  when '1' then
+  when 1 then
     puts Digest::MD5.hexdigest(word)
-  when '2' then
+  when 2 then
     puts Digest::SHA1.hexdigest(word)
+  when 3 then
+    puts Digest::SHA2.hexdigest(word)
 end
